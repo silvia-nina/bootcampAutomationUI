cc6010a39a8eb1b28667bfa90965bf9681596d87
silvianina <silvia.ninaramirez@gmail.com>
2022-11-15 15:39:28 -0400

    pruebita

---
+++ src/test/java/page/todoist/CenterSection.java
@@ -0,0 +1,12 @@
+package page.todoist;
+
+import control.Button;
+import control.Label;
+import org.openqa.selenium.By;
+
+public class CenterSection {
+
+    public Label todayLabel = new Label(By.xpath("//h1//span[text()='Today']"));
+
+
+}
